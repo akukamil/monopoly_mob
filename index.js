bec2c8f953de881d6859f06288e5cc24cb8b6663
akukamil <chamilioner_kid@mail.ru>
2025-10-22 15:40:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5331,11 +5331,6 @@ common={
 			sys_msg.add('Не ваша очередь!')
 			return
 		}
-
-		if(!my_turn_started){
-			sys_msg.add('Сначала нужно бросить кубики...')
-			return
-		}		
 		
 		coupons_dlg.activate()
 		
